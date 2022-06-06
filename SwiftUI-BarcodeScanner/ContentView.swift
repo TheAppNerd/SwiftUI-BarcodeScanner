2d0d8b1969eb22adf38eefff914715e9132c298c
Alex Thompson <Alex@appNerd.com.au>
2022-06-06 13:46:05 +1000

    Initial Commit

---
+++ SwiftUI-BarcodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Alexander Thompson on 6/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Alexander Thompson on 6/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
